4c2b0e90702761e91f3b01b56d712e4550a2620b
Edward Gilbert <egbiodiversity@gmail.com>
2016-07-21 11:41:03 -0500

    Adjustments to Image Cleaning functions
    - Extended thumbnail cleaning to build web image when thumbnail already
    exists

--- classes/ImageCleaner.php
+++ classes/ImageCleaner.php
@@ -114,8 +114,8 @@ class ImageCleaner{
 			$testSql = 'SELECT thumbnailurl, url FROM images WHERE (imgid = '.$imgId.') FOR UPDATE ';
 			$textRS = $this->conn->query($testSql);
 			if($testR = $textRS->fetch_object()){
-				if(!$testR->thumbnailurl || (substr($testR->thumbnailurl,0,10) == 'processing' && $testR->thumbnailurl != 'processing '.date('Y-m-d'))){
-					$tagSql = 'UPDATE images SET thumbnailurl = "processing '.date('Y-m-d').'" '.
+				if(!$testR->thumbnailurl || ($testR->url == 'empty') || (substr($testR->url,0,10) == 'processing' && $testR->url != 'processing '.date('Y-m-d'))){
+					$tagSql = 'UPDATE images SET url = "processing '.date('Y-m-d').'" '.
 						'WHERE (imgid = '.$imgId.')';
 					$this->conn->query($tagSql);
 				}
