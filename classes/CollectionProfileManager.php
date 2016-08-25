24648d6a2fb181ea24d7fbf264367953db069ea8
bdbrandt <benjamin.brandt@nau.edu>
2016-08-24 20:01:11 -0500

    Minor fix to update to Darwin Core Archive publishing page that adds link out to collection's iDigBio dataset page for collections publishing to iDigBio.

--- classes/CollectionProfileManager.php
+++ classes/CollectionProfileManager.php
@@ -590,7 +590,6 @@ class CollectionProfileManager {
         $url .= ($_SERVER['HTTPS']?'https://':'http://');
         $url .= $_SERVER['HTTP_HOST'].$CLIENT_ROOT;
         $url .= '/webservices/dwc/'.$guid.'}';
-        echo $url;
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -598,7 +597,7 @@ class CollectionProfileManager {
         $returnArr = json_decode($result,true);
 
         if(isset($returnArr['items'][0]['uuid'])){
-            //$this->idigbioKey = $returnArr['items'][0]['uuid'];
+            $this->idigbioKey = $returnArr['items'][0]['uuid'];
         }
         return $this->idigbioKey;
     }
